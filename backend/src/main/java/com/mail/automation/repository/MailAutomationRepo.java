0927c710d002a0d237bff256101d968a858f8331
JayaSree <websoc.dev@gmail.com>
2024-11-22 19:04:17 +0530

    feat:created repository file

---
+++ backend/src/main/java/com/mail/automation/repository/MailAutomationRepo.java
@@ -0,0 +1,11 @@
+package com.mail.automation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mail.automation.entity.Trainee;
+
+@Repository
+public interface MailAutomationRepo extends JpaRepository<Trainee,Integer>{
+
+}
